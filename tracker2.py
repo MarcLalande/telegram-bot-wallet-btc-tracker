9c1d9bd4619b67eee6b1dd833dedb2690cac3cdf
Lalande <Marc.Lalande07@gmail.com>
2024-12-05 17:05:15 +0300

    v2

--- tracker2.py
+++ tracker2.py
@@ -1,72 +1,76 @@
 from typing import Final
 import telegram
-from background import keep_alive  #импорт функции для поддержки работоспособности
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, JobQueue, Updater
-import threading
 import time
 import requests
 import datetime
-from flask import Flask
-import os
+import requests
+import pytz
 
-keep_alive()
-TOKEN = #########################YOUR TOKEN
-BOT_USERNAME: Final =  #########################Your bot name
+#logging.basicConfig(level=logging.DEBUG)
+TOKEN = '7346472629:AAHKAXoGGf69sRh-ldY1ikna09lmiyjB93E'
+BOT_USERNAME: Final = '@mr_100_tracker_bot'
+my_id : Final = '1494792751'
 WALLET_ADDRESS_MR_100 = '1Ay8vMC7R1UbyCCZRVULMV7iQpHSAbguJP'
 API_ROUTE = 'https://blockchain.info/rawaddr/1Ay8vMC7R1UbyCCZRVULMV7iQpHSAbguJP'
+API_ROUTE2= 'https://mempool.space/api/address/1Ay8vMC7R1UbyCCZRVULMV7iQpHSAbguJP'
 count = 0
-import pytz
+
 
 local_timezone = pytz.timezone('Europe/Amsterdam')
 negative_transactions = []
 positive_transactions = []
 time_daily = datetime.time(hour=00, minute=00,
                            second=00).replace(tzinfo=local_timezone)
-import asyncio
 
 
-def initial_req_wallet():
+
+def InitialWalletData():
     global count
-    response = requests.get(API_ROUTE)
+    response = requests.get(API_ROUTE2)
     if response.status_code == 200:
         data = response.json()
-        previous_balance = data['final_balance'] / 1e8
+
+        income= data['chain_stats']['funded_txo_sum']
+        outcome =data['chain_stats']['spent_txo_sum']
+
+        previous_balance = (income-outcome)/1e8
         return previous_balance
     else:
         print('failed to get balance, will try again in 3 min.')
-        time.sleep(60 * 30)
+        time.sleep(60 * 3)
         count += 1
         if count == 2:
             previous_balance = 0
             return previous_balance
         else:
-            initial_req_wallet()
+            InitialWalletData()
 
 
-previous_balance = initial_req_wallet()
+previous_balance = InitialWalletData()
 
 
-async def fetch_data_balance(context: ContextTypes.DEFAULT_TYPE) -> None:
+async def fetchDataDiff(context: ContextTypes.DEFAULT_TYPE) -> None:
 
     global previous_balance
     global negative_transactions
     global positive_transactions
-    print('got here')
-    print(positive_transactions, 'positives', 'n', negative_transactions)
 
-    response = requests.get(API_ROUTE)
+    response = requests.get(API_ROUTE2)
     if response.status_code == 200:
         data = response.json()
-        new_balance = data[
-            'final_balance'] / 1e8  # Convert balance from satoshis to BTC
 
+        income= data['chain_stats']['funded_txo_sum']
+        outcome =data['chain_stats']['spent_txo_sum']
+
+        new_balance = (income-outcome)/1e8
         if previous_balance != new_balance:
             operation = abs(previous_balance - new_balance)
             if operation > 10:
                 positive_transactions.append(operation)
                 await bot.send_message(
-                    chat_id=-1002249261036,
+                    chat_id='mr100News',
                     text=
                     f'Mr 100 has bought : {abs(previous_balance - new_balance)} BTC'
                 )
@@ -74,7 +78,7 @@ async def fetch_data_balance(context: ContextTypes.DEFAULT_TYPE) -> None:
             elif operation < -10:
                 negative_transactions.append(operation)
                 await bot.send_message(
-                    chat_id=-1002249261036,
+                    chat_id='mr100News',
                     text=
                     f' Mr 100 has sold : {abs(previous_balance - new_balance)} BTC'
                 )
@@ -83,25 +87,11 @@ async def fetch_data_balance(context: ContextTypes.DEFAULT_TYPE) -> None:
 
         else:
 
-            print('nothing happening')
+            print('No changes in the wallet for now')
 
     else:
         print('Failed to fetch data from the API.')
 
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print('started function')
-
-    await update.message.reply_text('Getting')
-
-
-async def monitor_mr_100_command(update: Update,
-                                 context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('monitor mr100')
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! Need help!')
 
 
 async def daily_count(context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -110,22 +100,36 @@ async def daily_count(context: ContextTypes.DEFAULT_TYPE) -> None:
     global negative_transactions
     global positive_transactions
     await bot.send_message(
-        chat_id=-1002249261036,
+        chat_id='mr100News',
         text=
         f' Mr 100 has bought : {sum(positive_transactions)} and sold : {sum(negative_transactions)} BTC .  the result is {sum(positive_transactions)-sum(negative_transactions)}'
+    )
+    await bot.send_message(
+        chat_id=my_id,
+        text=
+        f' daily count has happened. See news'
     )
     negative_transactions = []
     positive_transactions = []
 
 
+# Usage
+
+message = "Bot is alive"
+#url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={my_id}&text={message}"
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print('bot is starting')
+    print('Bot has started...')
     message_type: str = update.message.chat.type
+async def informAdmin(context: ContextTypes.DEFAULT_TYPE):
+    now = datetime.datetime.now()
+    await bot.send_message(
+        chat_id=my_id,
+        text=
+        f'Bot is working. Current time is: {now.time()}'
+    )
 
 
-async def bot_is_working(context: ContextTypes.DEFAULT_TYPE) -> None:
-    print('Bot is working')
-    await asyncio.sleep(0)  # You can change the sleep duration as needed
 
 
 if __name__ == '__main__':
@@ -134,11 +138,14 @@ if __name__ == '__main__':
 
     j = app.job_queue
 
-    app.add_handler(CommandHandler('start', start_command))
-    app.add_handler(CommandHandler('about', monitor_mr_100_command))
-    j.run_repeating(bot_is_working, interval=3600 * 6, first=0)
-    j.run_repeating(fetch_data_balance, interval=30 * 60, first=0)
-    j.run_daily(daily_count, time_daily, days=tuple(range(7)))
+
+
+
+    j.run_repeating(fetchDataDiff, interval=30 * 60, first=0)#every 10 min print if changes have been made 
+    j.run_repeating(informAdmin, interval=60 * 60, first=0) # send data to specify user about bot working
+    j.run_daily(daily_count, time_daily, days=tuple(range(7))) #every day print changes from yesterday
+
+
 
     print('Polling....')
 
